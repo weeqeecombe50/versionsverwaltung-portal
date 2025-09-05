ddceb477120683cf6c37778c240cf31ea26b59a3
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-05 21:19:07 +0300

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // regex für js-Dateien
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    mode: 'development'
+};
